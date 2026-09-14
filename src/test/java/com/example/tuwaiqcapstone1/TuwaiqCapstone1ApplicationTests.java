1a7a6b0612ce67ee97e7316b6c1f6700558c0b30
Abdullah <ab.asmari00@gmail.com>
2026-09-14 10:13:22 +0300

    Add: all

---
+++ src/test/java/com/example/tuwaiqcapstone1/TuwaiqCapstone1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuwaiqcapstone1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuwaiqCapstone1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
