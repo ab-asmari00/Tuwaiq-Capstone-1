537b6b2e56d8d5c3f4c01e3ac8a2fd2e334dd11a
Abdullah <ab.asmari00@gmail.com>
2026-09-15 13:18:58 +0300

    Add: buyProdcut method to UserService

--- src/main/java/com/example/tuwaiqcapstone1/Service/UserService.java
+++ src/main/java/com/example/tuwaiqcapstone1/Service/UserService.java
@@ -1,14 +1,23 @@
 package com.example.tuwaiqcapstone1.Service;
 
+import com.example.tuwaiqcapstone1.Model.Merchant;
+import com.example.tuwaiqcapstone1.Model.MerchantStock;
+import com.example.tuwaiqcapstone1.Model.Product;
 import com.example.tuwaiqcapstone1.Model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    private ArrayList<User> users = new ArrayList<>();
+    private final MerchantService merchantService;
+    private final ProductService productService;
+    private final MerchantStockService merchantStockService;
+
+    ArrayList<User> users = new ArrayList<>();
 
     public ArrayList<User> getUsers() {
         return users;
@@ -38,6 +47,64 @@ public class UserService {
             }
         }
         return false;
+    }
+
+    public int buyProduct(String userId, String productId, String merchantId) {
+
+        User user = null;
+        for (User u : users) {
+            if (u.getId().equals(userId)) {
+                user = u;
+                break;
+            }
+        }
+        if (user == null) {
+            return 1; // UserId not found
+        }
+
+        Product product = null;
+        for (Product p : productService.getProducts()) {
+            if (product.getId().equals(productId)) {
+                product = p;
+                break;
+            }
+        }
+        if (product == null) {
+            return 2; // ProductId was not found
+        }
+
+        Merchant merchant = null;
+        for (Merchant m : merchantService.getMerchants()) {
+            if (m.getId().equals(merchantId)) {
+                merchant = m;
+                break;
+            }
+        }
+        if (merchant == null) {
+            return 3; // MerchantId was not found
+        }
+
+        MerchantStock stock = null;
+        for (MerchantStock ms : merchantStockService.getMerchantStocks()) {
+            if (ms.getProductId().equals(productId) &&
+                ms.getMerchantId().equals(merchantId)) {
+                stock = ms;
+                break;
+            }
+        }
+
+        if (stock.getStock() < 1) {
+            return 4; // wanted Product is out of stock
+        }
+
+        if (user.getBalance() < product.getPrice()) {
+            return 5; // Balance is insufficient to buy this product
+        }
+
+        stock.setStock(stock.getStock() - 1);
+        user.setBalance(user.getBalance() - product.getPrice());
+
+        return 0; // Purchase done successfully
     }
 
 }
